b79ca69cc209ead93bb43442f1acf57985b8e55a
brandtagai <brandtagai@gmail.com>
2025-09-26 12:43:14 -0400

    Update BottomNav.jsx

--- src/components/navigation/BottomNav.jsx
+++ src/components/navigation/BottomNav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { createPageUrl } from "@/utils";
+import { createPageUrl } from "../../utils";
 
 // Simple SVG icons to replace lucide-react
 const Settings = ({ className }) => (
