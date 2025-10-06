54ed88c2567c03571d3b2e011977b0e4aeab7406
brandtagai <brandtagai@gmail.com>
2025-10-06 17:48:25 -0400

    Update Pawall.jsx

--- pages/Pawall.jsx
+++ pages/Pawall.jsx
@@ -1,10 +1,9 @@
-
 import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { useNavigate } from "react-router-dom";
 import { createPageUrl } from "@/utils";
-import { Purchase } from "@/entities/Purchase";
+import { Purchase } from "@/entities/mockEntities";
 import { 
   Crown, 
   Zap, 
